47ff4e5e526cb6e253a6a44da0bb1f617ece7f9f
Андрей Литвинов <busy4beaver@gmail.com>
2026-08-03 09:13:22 +0700

    feat: annotate proxy_property batch remaining 2/20

--- simpleautocad/Autocad/Entities/AcadOle.py
+++ simpleautocad/Autocad/Entities/AcadOle.py
@@ -8,16 +8,16 @@ class AcadOle(AcadEntity):
     def __init__(self, obj) -> None:
         super().__init__(obj)
 
-    Height = proxy_property(float, 'Height', AccessMode.ReadWrite)
-    InsertionPoint = proxy_property('PyGePoint3d', 'InsertionPoint', AccessMode.ReadWrite)
-    LockAspectRatio = proxy_property(bool, 'LockAspectRatio', AccessMode.ReadWrite)
-    OleItemType = proxy_property('AcOleType', 'OleItemType', AccessMode.ReadWrite)
-    OlePlotQuality = proxy_property('AcOlePlotQuality', 'OlePlotQuality', AccessMode.ReadWrite)
-    OleSourceApp = proxy_property(str, 'OleSourceApp', AccessMode.ReadWrite)
-    Rotation = proxy_property(float, 'Rotation', AccessMode.ReadWrite)
-    ScaleHeight = proxy_property(float, 'ScaleHeight', AccessMode.ReadWrite)
-    ScaleWidth = proxy_property(float, 'ScaleWidth', AccessMode.ReadWrite)
-    Width = proxy_property(float, 'Width', AccessMode.ReadWrite)
+    Height: float = proxy_property(float, 'Height', AccessMode.ReadWrite)
+    InsertionPoint: PyGePoint3d = proxy_property('PyGePoint3d', 'InsertionPoint', AccessMode.ReadWrite)
+    LockAspectRatio: bool = proxy_property(bool, 'LockAspectRatio', AccessMode.ReadWrite)
+    OleItemType: AcOleType = proxy_property('AcOleType', 'OleItemType', AccessMode.ReadWrite)
+    OlePlotQuality: AcOlePlotQuality = proxy_property('AcOlePlotQuality', 'OlePlotQuality', AccessMode.ReadWrite)
+    OleSourceApp: str = proxy_property(str, 'OleSourceApp', AccessMode.ReadWrite)
+    Rotation: float = proxy_property(float, 'Rotation', AccessMode.ReadWrite)
+    ScaleHeight: float = proxy_property(float, 'ScaleHeight', AccessMode.ReadWrite)
+    ScaleWidth: float = proxy_property(float, 'ScaleWidth', AccessMode.ReadWrite)
+    Width: float = proxy_property(float, 'Width', AccessMode.ReadWrite)
 
     def Copy(self) -> AcadOle:
         return AcadOle(self._obj.Copy())
--- simpleautocad/Autocad/Objects/AcadPlot.py
+++ simpleautocad/Autocad/Objects/AcadPlot.py
@@ -9,10 +9,10 @@ class AcadPlot(AppObject):
     def __init__(self, obj) -> None:
         super().__init__(obj)
 
-    Application = proxy_property('AcadApplication', 'Application', AccessMode.ReadOnly)
-    BatchPlotProgress = proxy_property(bool, 'BatchPlotProgress', AccessMode.ReadWrite)
-    NumberOfCopies = proxy_property(int, 'NumberOfCopies', AccessMode.ReadWrite)
-    QuietErrorMode = proxy_property(bool, 'QuietErrorMode', AccessMode.ReadWrite)
+    Application: AcadApplication = proxy_property('AcadApplication', 'Application', AccessMode.ReadOnly)
+    BatchPlotProgress: bool = proxy_property(bool, 'BatchPlotProgress', AccessMode.ReadWrite)
+    NumberOfCopies: int = proxy_property(int, 'NumberOfCopies', AccessMode.ReadWrite)
+    QuietErrorMode: bool = proxy_property(bool, 'QuietErrorMode', AccessMode.ReadWrite)
 
     def DisplayPlotPreview(self, Preview) -> None:
         self._obj.DisplayPlotPreview(Preview)
