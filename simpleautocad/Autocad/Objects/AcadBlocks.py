01b804d4577447a9fda7dd62d723f1f1ea7c9683
Андрей Литвинов <busy4beaver@gmail.com>
2026-08-13 15:32:51 +0700

    fix: AcadBlocks as IAcadObjectCollection[AcadBlock]

--- simpleautocad/Autocad/Objects/AcadBlocks.py
+++ simpleautocad/Autocad/Objects/AcadBlocks.py
@@ -1,20 +1,22 @@
 from __future__ import annotations
 
+from typing import Iterator
+
 from ..AcadObject import IAcadObjectCollection
 from ...Types.Ge import PyGePoint3d
 from .AcadBlock import AcadBlock
 
 
-class AcadBlocks(IAcadObjectCollection):
+class AcadBlocks(IAcadObjectCollection[AcadBlock]):
     def __init__(self, obj) -> None:
         super().__init__(obj)
 
     def Add(self, InsertionPoint: PyGePoint3d, Name: str) -> AcadBlock:
         return AcadBlock(self._obj.Add(InsertionPoint(), Name))
 
-    def Item(self, Index: int) -> AcadBlock:
+    def Item(self, Index: int | str) -> AcadBlock:
         return AcadBlock(self._obj.Item(Index))
 
-    def __iter__(self):
+    def __iter__(self) -> Iterator[AcadBlock]:
         for item in self._obj:
             yield AcadBlock(item)
