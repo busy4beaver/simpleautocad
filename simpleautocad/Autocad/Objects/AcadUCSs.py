ad6c36ad7d82a5761435ecff94920ce9aacabb51
Андрей Литвинов <busy4beaver@gmail.com>
2026-08-13 15:33:46 +0700

    fix: AcadUCSs as IAcadObjectCollection[AcadUCS]

--- simpleautocad/Autocad/Objects/AcadUCSs.py
+++ simpleautocad/Autocad/Objects/AcadUCSs.py
@@ -1,11 +1,13 @@
 from __future__ import annotations
 
+from typing import Iterator
+
 from ..AcadObject import IAcadObjectCollection
 from ...Types.Ge import PyGePoint3d
 from .AcadUCS import AcadUCS
 
 
-class AcadUCSs(IAcadObjectCollection):
+class AcadUCSs(IAcadObjectCollection[AcadUCS]):
     def __init__(self, obj) -> None:
         super().__init__(obj)
 
@@ -18,9 +20,9 @@ class AcadUCSs(IAcadObjectCollection):
     ) -> AcadUCS:
         return AcadUCS(self._obj.Add(Origin(), XAxisPoint(), YAxisPoint(), Name))
 
-    def Item(self, Index: int) -> AcadUCS:
+    def Item(self, Index: int | str) -> AcadUCS:
         return AcadUCS(self._obj.Item(Index))
 
-    def __iter__(self):
+    def __iter__(self) -> Iterator[AcadUCS]:
         for item in self._obj:
             yield AcadUCS(item)
