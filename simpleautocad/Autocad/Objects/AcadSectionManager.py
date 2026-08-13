75d8573a6309b4ba64e55b526361368ddc2c08e5
Андрей Литвинов <busy4beaver@gmail.com>
2026-08-13 15:34:05 +0700

    fix: AcadSectionManager as IAcadObjectCollection[AcadSection]

--- simpleautocad/Autocad/Objects/AcadSectionManager.py
+++ simpleautocad/Autocad/Objects/AcadSectionManager.py
@@ -1,10 +1,12 @@
 from __future__ import annotations
 
+from typing import Iterator
+
 from ..AcadObject import IAcadObjectCollection
 from ..Entities.AcadSection import AcadSection
 
 
-class AcadSectionManager(IAcadObjectCollection):
+class AcadSectionManager(IAcadObjectCollection[AcadSection]):
     def __init__(self, obj) -> None:
         super().__init__(obj)
 
@@ -13,3 +15,10 @@ class AcadSectionManager(IAcadObjectCollection):
 
     def GetUniqueSectionName(self) -> str:
         return self._obj.GetUniqueSectionName()
+
+    def Item(self, Index: int | str) -> AcadSection:
+        return AcadSection(self._obj.Item(Index))
+
+    def __iter__(self) -> Iterator[AcadSection]:
+        for item in self._obj:
+            yield AcadSection(item)
