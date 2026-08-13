e902bce39fd2fd883e62cfdfde6df487f8ce17c2
Андрей Литвинов <busy4beaver@gmail.com>
2026-08-13 15:33:28 +0700

    fix: AcadMaterials as IAcadObjectCollection[AcadMaterial]

--- simpleautocad/Autocad/Objects/AcadMaterials.py
+++ simpleautocad/Autocad/Objects/AcadMaterials.py
@@ -1,19 +1,21 @@
 from __future__ import annotations
 
+from typing import Iterator
+
 from ..AcadObject import IAcadObjectCollection
 from .AcadMaterial import AcadMaterial
 
 
-class AcadMaterials(IAcadObjectCollection):
+class AcadMaterials(IAcadObjectCollection[AcadMaterial]):
     def __init__(self, obj) -> None:
         super().__init__(obj)
 
     def Add(self, Name: str) -> AcadMaterial:
         return AcadMaterial(self._obj.Add(Name))
 
-    def Item(self, Index: int) -> AcadMaterial:
+    def Item(self, Index: int | str) -> AcadMaterial:
         return AcadMaterial(self._obj.Item(Index))
 
-    def __iter__(self):
+    def __iter__(self) -> Iterator[AcadMaterial]:
         for item in self._obj:
             yield AcadMaterial(item)
