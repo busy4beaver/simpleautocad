68cc84a1819b3173253637daf23824f94084fbc4
Андрей Литвинов <busy4beaver@gmail.com>
2026-08-13 15:33:24 +0700

    fix: AcadLayouts as IAcadObjectCollection[AcadLayout]

--- simpleautocad/Autocad/Objects/AcadLayouts.py
+++ simpleautocad/Autocad/Objects/AcadLayouts.py
@@ -1,20 +1,22 @@
 from __future__ import annotations
 
+from typing import Iterator
+
 from ..AcadObject import IAcadObjectCollection
 from ...Types.Ge import PyGePoint3d
 from .AcadLayout import AcadLayout
 
 
-class AcadLayouts(IAcadObjectCollection):
+class AcadLayouts(IAcadObjectCollection[AcadLayout]):
     def __init__(self, obj) -> None:
         super().__init__(obj)
 
     def Add(self, InsertionPoint: PyGePoint3d, Name: str) -> AcadLayout:
         return AcadLayout(self._obj.Add(InsertionPoint(), Name))
 
-    def Item(self, Index: int) -> AcadLayout:
+    def Item(self, Index: int | str) -> AcadLayout:
         return AcadLayout(self._obj.Item(Index))
 
-    def __iter__(self):
+    def __iter__(self) -> Iterator[AcadLayout]:
         for item in self._obj:
             yield AcadLayout(item)
