fac4450d25e6fdbf1dc6675c2daac1a7c06fb859
Андрей Литвинов <busy4beaver@gmail.com>
2026-08-13 15:08:47 +0700

    fix: Optional annotation on TextStyles.Add

--- simpleautocad/Autocad/Objects/AcadTextStyles.py
+++ simpleautocad/Autocad/Objects/AcadTextStyles.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from typing import Iterator, Optional
+
 from ..AcadObject import IAcadObjectCollection
 from .AcadTextStyle import AcadTextStyle
 
@@ -8,14 +10,14 @@ class AcadTextStyles(IAcadObjectCollection):
     def __init__(self, obj) -> None:
         super().__init__(obj)
 
-    def Add(self, Name: str = None) -> AcadTextStyle:
+    def Add(self, Name: Optional[str] = None) -> AcadTextStyle:
         if Name is None:
             return AcadTextStyle(self._obj.Add())
         return AcadTextStyle(self._obj.Add(Name))
 
-    def Item(self, Index: int) -> AcadTextStyle:
+    def Item(self, Index: int | str) -> AcadTextStyle:
         return AcadTextStyle(self._obj.Item(Index))
 
-    def __iter__(self):
+    def __iter__(self) -> Iterator[AcadTextStyle]:
         for item in self._obj:
             yield AcadTextStyle(item)
