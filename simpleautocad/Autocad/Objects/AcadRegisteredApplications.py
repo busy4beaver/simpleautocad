c9395c155ee6cdbe65665fcf9d9b014144e7164a
Андрей Литвинов <busy4beaver@gmail.com>
2026-08-13 15:33:41 +0700

    fix: AcadRegisteredApplications as typed collection

--- simpleautocad/Autocad/Objects/AcadRegisteredApplications.py
+++ simpleautocad/Autocad/Objects/AcadRegisteredApplications.py
@@ -1,19 +1,21 @@
 from __future__ import annotations
 
+from typing import Iterator
+
 from ..AcadObject import IAcadObjectCollection
 from .AcadRegisteredApplication import AcadRegisteredApplication
 
 
-class AcadRegisteredApplications(IAcadObjectCollection):
+class AcadRegisteredApplications(IAcadObjectCollection[AcadRegisteredApplication]):
     def __init__(self, obj) -> None:
         super().__init__(obj)
 
     def Add(self, Name: str) -> AcadRegisteredApplication:
         return AcadRegisteredApplication(self._obj.Add(Name))
 
-    def Item(self, Index: int) -> AcadRegisteredApplication:
+    def Item(self, Index: int | str) -> AcadRegisteredApplication:
         return AcadRegisteredApplication(self._obj.Item(Index))
 
-    def __iter__(self):
+    def __iter__(self) -> Iterator[AcadRegisteredApplication]:
         for item in self._obj:
             yield AcadRegisteredApplication(item)
