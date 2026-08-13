5cc4a94446e1b89a14e426856e0e9a7be2fefc50
Андрей Литвинов <busy4beaver@gmail.com>
2026-08-13 15:32:59 +0700

    fix: AcadViews as IAcadObjectCollection[AcadView]

--- simpleautocad/Autocad/Objects/AcadViews.py
+++ simpleautocad/Autocad/Objects/AcadViews.py
@@ -1,10 +1,12 @@
 from __future__ import annotations
 
+from typing import Iterator
+
 from ..AcadObject import IAcadObjectCollection
 from .AcadView import AcadView
 
 
-class AcadViews(IAcadObjectCollection):
+class AcadViews(IAcadObjectCollection[AcadView]):
     def __init__(self, obj) -> None:
         super().__init__(obj)
 
@@ -14,6 +16,6 @@ class AcadViews(IAcadObjectCollection):
     def Item(self, Index: int | str) -> AcadView:
         return AcadView(self._obj.Item(Index))
 
-    def __iter__(self):
+    def __iter__(self) -> Iterator[AcadView]:
         for item in self._obj:
             yield AcadView(item)
