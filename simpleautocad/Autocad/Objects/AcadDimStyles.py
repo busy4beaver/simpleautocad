144782d15a5a53d74e9e29d99601be74c6fa93f2
Андрей Литвинов <busy4beaver@gmail.com>
2026-08-13 15:33:32 +0700

    fix: AcadDimStyles as IAcadObjectCollection[AcadDimStyle]

--- simpleautocad/Autocad/Objects/AcadDimStyles.py
+++ simpleautocad/Autocad/Objects/AcadDimStyles.py
@@ -1,19 +1,21 @@
 from __future__ import annotations
 
+from typing import Iterator
+
 from ..AcadObject import IAcadObjectCollection
 from .AcadDimStyle import AcadDimStyle
 
 
-class AcadDimStyles(IAcadObjectCollection):
+class AcadDimStyles(IAcadObjectCollection[AcadDimStyle]):
     def __init__(self, obj) -> None:
         super().__init__(obj)
 
     def Add(self, Name: str) -> AcadDimStyle:
         return AcadDimStyle(self._obj.Add(Name))
 
-    def Item(self, Index: int) -> AcadDimStyle:
+    def Item(self, Index: int | str) -> AcadDimStyle:
         return AcadDimStyle(self._obj.Item(Index))
 
-    def __iter__(self):
+    def __iter__(self) -> Iterator[AcadDimStyle]:
         for item in self._obj:
             yield AcadDimStyle(item)
