9e30b7a76c6e8f587504287e73be301d28f91380
Андрей Литвинов <busy4beaver@gmail.com>
2026-08-13 15:08:35 +0700

    fix: Optional annotation and __iter__ for AcadDictionaries

--- simpleautocad/Autocad/Objects/AcadDictionaries.py
+++ simpleautocad/Autocad/Objects/AcadDictionaries.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from typing import Iterator, Optional
+
 from ..AcadObject import IAcadObjectCollection
 from .AcadDictionary import AcadDictionary
 
@@ -8,7 +10,14 @@ class AcadDictionaries(IAcadObjectCollection):
     def __init__(self, obj) -> None:
         super().__init__(obj)
 
-    def Add(self, Name: str = None) -> AcadDictionary:
+    def Add(self, Name: Optional[str] = None) -> AcadDictionary:
         if Name is None:
             return AcadDictionary(self._obj.Add())
         return AcadDictionary(self._obj.Add(Name))
+
+    def Item(self, Index: int | str) -> AcadDictionary:
+        return AcadDictionary(self._obj.Item(Index))
+
+    def __iter__(self) -> Iterator[AcadDictionary]:
+        for item in self._obj:
+            yield AcadDictionary(item)
