afb30094b25cbac7ac4b58bcd5bf79bd067a19ef
Андрей Литвинов <busy4beaver@gmail.com>
2026-08-13 15:08:52 +0700

    fix: Optional annotation on PlotConfigurations.Add

--- simpleautocad/Autocad/Objects/AcadPlotConfigurations.py
+++ simpleautocad/Autocad/Objects/AcadPlotConfigurations.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from typing import Iterator, Optional
+
 from ..AcadObject import IAcadObjectCollection
 from .AcadPlotConfiguration import AcadPlotConfiguration
 
@@ -8,7 +10,7 @@ class AcadPlotConfigurations(IAcadObjectCollection):
     def __init__(self, obj) -> None:
         super().__init__(obj)
 
-    def Add(self, Name: str, ModelType: bool = None) -> AcadPlotConfiguration:
+    def Add(self, Name: str, ModelType: Optional[bool] = None) -> AcadPlotConfiguration:
         if ModelType is not None:
             return AcadPlotConfiguration(self._obj.Add(Name, ModelType))
         return AcadPlotConfiguration(self._obj.Add(Name))
@@ -16,6 +18,6 @@ class AcadPlotConfigurations(IAcadObjectCollection):
     def Item(self, Index: int | str) -> AcadPlotConfiguration:
         return AcadPlotConfiguration(self._obj.Item(Index))
 
-    def __iter__(self):
+    def __iter__(self) -> Iterator[AcadPlotConfiguration]:
         for item in self._obj:
             yield AcadPlotConfiguration(item)
