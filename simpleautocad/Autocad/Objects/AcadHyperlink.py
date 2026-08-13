f055e5ec71a76e5a7ac6e8064f9cc176f18d3012
Андрей Литвинов <busy4beaver@gmail.com>
2026-08-13 15:06:23 +0700

    fix: add Delete/Application and complete annotations on AcadHyperlink

--- simpleautocad/Autocad/Objects/AcadHyperlink.py
+++ simpleautocad/Autocad/Objects/AcadHyperlink.py
@@ -1,13 +1,24 @@
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+
 from ..Proxy import proxy_property, AccessMode
 from ..Base import AppObject
 
+if TYPE_CHECKING:
+    from .AcadApplication import AcadApplication
+
 
 class AcadHyperlink(AppObject):
+    """A URL and optional description / named location on an entity."""
+
     def __init__(self, obj) -> None:
         super().__init__(obj)
 
+    Application: AcadApplication = proxy_property('AcadApplication', 'Application', AccessMode.ReadOnly)
     URL: str = proxy_property(str, 'URL', AccessMode.ReadWrite)
     URLDescription: str = proxy_property(str, 'URLDescription', AccessMode.ReadWrite)
     URLNamedLocation: str = proxy_property(str, 'URLNamedLocation', AccessMode.ReadWrite)
+
+    def Delete(self) -> None:
+        self._obj.Delete()
