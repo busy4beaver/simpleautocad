4a3bd7f05b7e9d919976c60fc6fec685a5a8d77a
Андрей Литвинов <busy4beaver@gmail.com>
2026-08-13 15:32:55 +0700

    fix: AcadGroups as IAcadObjectCollection[AcadGroup]

--- simpleautocad/Autocad/Objects/AcadGroups.py
+++ simpleautocad/Autocad/Objects/AcadGroups.py
@@ -1,10 +1,12 @@
 from __future__ import annotations
 
+from typing import Iterator
+
 from ..AcadObject import IAcadObjectCollection
 from .AcadGroup import AcadGroup
 
 
-class AcadGroups(IAcadObjectCollection):
+class AcadGroups(IAcadObjectCollection[AcadGroup]):
     def __init__(self, obj) -> None:
         super().__init__(obj)
 
@@ -14,6 +16,6 @@ class AcadGroups(IAcadObjectCollection):
     def Item(self, Index: int | str) -> AcadGroup:
         return AcadGroup(self._obj.Item(Index))
 
-    def __iter__(self):
+    def __iter__(self) -> Iterator[AcadGroup]:
         for item in self._obj:
             yield AcadGroup(item)
